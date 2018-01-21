d7ad80fd7291115d0827fd88542f833c40103010
Matthew Seal <1863892+MSeal@users.noreply.github.com>
2018-01-21 15:31:06 -0800

    Added language translations

--- DireRaids/DireRaids.cs
+++ DireRaids/DireRaids.cs
@@ -1,15 +1,56 @@
-﻿using RimWorld;
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
 using Verse;
+using HugsLib;
+using HugsLib.Settings;
 
 namespace DireRaids {
-  namespace RimWorld {
-    public class IncidentWorker_DireRaidEnemy : IncidentWorker_RaidEnemy {
-      public const int POINT_MULTIPLIER = 3;
+  [StaticConstructorOnStartup]
+  public class IncidentWorker_DireRaidEnemy : IncidentWorker_RaidEnemy {
+    protected static Func<float> getPointMultiplier;
 
-      protected override void ResolveRaidPoints(IncidentParms parms) {
-        base.ResolveRaidPoints(parms);
-        parms.points *= POINT_MULTIPLIER;
+    static IncidentWorker_DireRaidEnemy() {
+      // load order does not matter- HugsLib initializes before StaticConstructorOnStartup
+      InitializePointMultiplier();
+      Log.Message("Dire Raids: Setting point multiplier: " + getPointMultiplier());
+    }
+
+    protected static void InitializePointMultiplier() {
+      const float default_point_multiplier = 3.0F;
+
+      // Need a wrapper method/lambda to be able to catch the TypeLoadException when HugsLib isn't present
+      try {
+        ((Action) (() => {
+          var settings = HugsLibController.Instance.Settings.GetModSettings("DireRaids");
+          // add a mod name to display in the Mods Settings menu
+          settings.EntryName = "DireRaids.SettingName".Translate();
+          // handle can't be saved as a SettingHandle<> type; otherwise the compiler generated closure class will throw a typeloadexception
+          object handle = settings.GetHandle(
+            "pointMultiplier",
+            "DireRaids.DangerSetting".Translate(),
+            "DireRaids.DangerSettingDescription".Translate(),
+            default_point_multiplier);
+          getPointMultiplier = () => (SettingHandle<float>) handle;
+        }))();
+      } catch (TypeLoadException) {
+        getPointMultiplier = () => default_point_multiplier;
       }
+    }
+
+    protected override void ResolveRaidPoints(IncidentParms parms) {
+      base.ResolveRaidPoints(parms);
+      float originPoints = parms.points;
+      parms.points *= getPointMultiplier();
+      Log.Message("Dire Raid points recalculated: " + originPoints + " to " + parms.points);
+    }
+
+    protected override string GetLetterLabel(IncidentParms parms) {
+      return "DireRaidEnemy.letterLabel".Translate();
+    }
+
+    protected override string GetLetterText(IncidentParms parms, List<Pawn> pawns) {
+      return "DireRaidEnemy.letterText".Translate() + "\n\n" + base.GetLetterText(parms, pawns);
     }
   }
 }
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TARGET_DIR="/mnt/c/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods/Dire Raids"
+[ -d "${TARGET_DIR}" ] || mkdir "${TARGET_DIR}"
+cp -r About "${TARGET_DIR}"
+cp -r Assemblies "${TARGET_DIR}"
+cp -r Defs "${TARGET_DIR}"
+cp -r Languages "${TARGET_DIR}"
