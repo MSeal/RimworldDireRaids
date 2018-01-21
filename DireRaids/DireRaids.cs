c454abf3dddd905c9daaa332226fd01e9c9b5bc5
Matthew Seal <1863892+MSeal@users.noreply.github.com>
2018-01-20 20:45:26 -0800

    Added initial project files

---
+++ DireRaids/DireRaids.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DireRaids {
+  namespace RimWorld {
+    public class IncidentWorker_DireRaidEnemy : IncidentWorker_RaidEnemy {
+      public const int POINT_MULTIPLIER = 3;
+
+      protected override void ResolveRaidPoints(IncidentParms parms) {
+        base.ResolveRaidPoints(parms);
+        parms.points *= POINT_MULTIPLIER;
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# RimworldDireRaids
-A small mod adding a new Raid event to Rimworld
+# Dire Raids
+
+Version: 0.18.0
+
+# Description
+Adds a rare but dangerous event, the Dire Raid. Meant to challenge even well developed bases this mod adds some risk to mid and late game development.
+
+# FAQ
+*Is the mod save safe?*
+Yes install or remove it from existings saves without risk.
